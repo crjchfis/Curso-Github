22aa526289341b712531a01f84d48ab4b0fd3a9c
crjchfis <crjchfis@hotmail.com>
2016-03-08 22:41:57 -0500

    Update hm.java

--- hm.java
+++ hm.java
@@ -2,6 +2,7 @@ public class Hm
 {
   public static void main(String args[])
   {
-    System.out.print("Hola Mundo");
+    System.out.print("Facultad de Ingenieria de Sistemas");
+    System.out.print("Alumno: Cristian J. hacalatana Fajardo");
   } 
 } //Fin de la clase Hm
