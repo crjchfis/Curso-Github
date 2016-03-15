498984b1b46954c2c1545d7b8b07d5f3c228b2fa
crjchfis <crjchfis@hotmail.com>
2016-03-14 22:34:10 -0500

    Prueba de pull

--- hm.java
+++ hm.java
@@ -4,5 +4,7 @@ public class Hm
   {
     System.out.print("Facultad de Ingenieria de Sistemas");
     System.out.print("Alumno: Cristian J. Chacaltana Fajardo");
+    
+    'Derecho Reservado de Cristian J. Chacaltana Fajardo - FIS 2016'
   } 
 } //Fin de la clase Hm
