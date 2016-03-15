809701ca932e58b8f171ffcf9475c8c77c592e20
crjchf <crjchf@hotmail.com>
2016-03-14 22:06:50 -0500

    commit para prueba de push

--- hm.java
+++ hm.java
@@ -3,6 +3,6 @@ public class Hm
   public static void main(String args[])
   {
     System.out.print("Facultad de Ingenieria de Sistemas");
-    System.out.print("Alumno: Cristian J. hacalatana Fajardo");
+    System.out.print("Alumno: Cristian J. Chacalatana Fajardo");
   } 
 } //Fin de la clase Hm
