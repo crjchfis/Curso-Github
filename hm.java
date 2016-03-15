879b88e1b8e91a8edd1fdf19ba124f1b83e07709
crjchf <crjchf@hotmail.com>
2016-03-14 22:09:54 -0500

    Correcion del nombre del programador

--- hm.java
+++ hm.java
@@ -3,6 +3,6 @@ public class Hm
   public static void main(String args[])
   {
     System.out.print("Facultad de Ingenieria de Sistemas");
-    System.out.print("Alumno: Cristian J. Chacalatana Fajardo");
+    System.out.print("Alumno: Cristian J. Chacaltana Fajardo");
   } 
 } //Fin de la clase Hm
