f110508afb32efb6e2d2fc0e6685febb12fcd4f2
crjchfis <crjchfis@hotmail.com>
2016-03-17 22:15:11 -0500

    prueba

--- hm.java
+++ hm.java
@@ -8,3 +8,15 @@ public class Hm
     'Derecho Reservado de Cristian J. Chacaltana Fajardo - FIS 2016'
   } 
 } //Fin de la clase Hm
+
+/*
+ * Ejercicio N° 1
+ * Escriba un programa que imprima en la pantalla su nombre completo en una línea y en la linea siguiente se fecha de nacimeitno
+ * y en la línea siguiente su fecha de nacimiento.
+ */
+ 
+ 
+ /*
+ * Ejercicio N° 1
+ * Escriba un programa que imprima en la pantalla su nombre completo en una línea y en la línea siguiente su fecha de nacimiento.
+ */
